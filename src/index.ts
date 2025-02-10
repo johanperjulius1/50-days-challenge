47422ab5b97379513537e17c2771503577fcd2c5
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-02-10 19:46:19 +0100

    testing solutions

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import "./style.css";
 
-const imageContainers = document.querySelectorAll<HTMLDivElement>('.image');
+const imageContainers = document.querySelectorAll<HTMLDivElement>('.image-container');
 
 imageContainers.forEach((container) => {
     container.addEventListener('click', () => {
- 
-
         if (container.classList.contains('active')) {
             container.classList.remove('active');
+            const imageTitle = container.querySelector('h3');
+            if (imageTitle) imageTitle.classList.remove('active');
         } else {
-            imageContainers.forEach((c) => c.classList.remove('active'));
+            imageContainers.forEach((c) => {
+                c.classList.remove('active');
+                const imageTitle = c.querySelector('h3');
+                if (imageTitle) imageTitle.classList.remove('active');
+            });
             container.classList.add('active');
+            const imageTitle = container.querySelector('h3');
+            if (imageTitle) imageTitle.classList.add('active');
         }
     });
 });
