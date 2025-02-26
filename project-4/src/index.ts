c69d4840554bfd03aaaf92eee87f0b819be05ce1
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-02-26 22:58:38 +0100

    committting all

--- project-4/src/index.ts
+++ project-4/src/index.ts
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const inputField = document.querySelector(".search") as HTMLElement;
+const inputContainer = document.querySelector(".input-container") as HTMLElement;
+
+inputContainer.addEventListener('click', function() { 
+    console.log("input icon was clicked")
+    this.classList.toggle('active')
+ });
+
+inputField.addEventListener('click', function() { 
+    console.log("input icon was clicked")
+    this.classList.toggle('active')
+ });
