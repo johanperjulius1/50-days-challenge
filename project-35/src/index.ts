8497f01e13a4c9337c3930f2a35983202f0d8576
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-03-10 16:14:39 +0100

    finishing image carousel

--- project-35/src/index.ts
+++ project-35/src/index.ts
@@ -1,1 +1,44 @@
 import "./style.css";
+
+const imgs = document.querySelector(".image-container") as HTMLElement;
+const leftButton = document.querySelector("#left") as HTMLElement;
+const rightButton = document.querySelector("#right") as HTMLElement;
+const allImages = document.querySelectorAll("#imgs img") as NodeList;
+
+let interval = setInterval(run, 2000);
+let index = 0;
+
+function run() {
+    index++;
+    changeImage();
+}
+
+function changeImage() {
+    if (index > allImages.length - 1) {
+        index = 0;
+    } else if (index < 0) {
+        index = allImages.length - 1;
+    }
+
+    if (imgs) {
+        imgs.style.transform = `translateX(${-index * 500}px)`;
+    }
+}
+
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+rightButton.addEventListener('click', () => {
+    index++
+    changeImage()
+    resetInterval()
+})
+
+leftButton.addEventListener('click', () => {
+    index--
+    changeImage()
+    resetInterval()
+})
